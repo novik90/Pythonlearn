42aa672a1eba432abc3b7ce8a6f0c2316ce44ce1
Ivan Novikov <ivan.novikov.1990@list.ru>
2019-11-17 15:26:22 +0300

    fixes

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ from datetime import datetime
 import time
 import random
 
-def lesson_one():
+
+def lesson_1():
     odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21,
             23, 25, 27, 29, 31, 33, 35, 37, 41,
             43, 45, 47, 49, 51, 53, 55, 57, 59]
@@ -19,11 +20,12 @@ def lesson_one():
         if i != 5:
             time.sleep(wait_time)
 
-def lesson_two():
+
+def lesson_2():
     word = "bottles"
 
     for beer_num in range(99, 0, -1):
-        print beer_num, word, "of beer on the wall."
+        print(beer_num, word, "of beer on the wall.")
         print(beer_num, word, "of beer.")
         print("Take one down.")
         print("Paste around.")
@@ -44,9 +46,10 @@ def factorial(x):
     elif x > 10:
         return "number so lagre"
     else:
-        return x * factorial(x-1)
+        return x * factorial(x - 1)
 
-def lesson_three():
+
+def lesson_3():
     vowels = ['a', 'e', 'i', 'o', 'u']
     word = input("write the word... ")
     found = []
@@ -57,7 +60,8 @@ def lesson_three():
     for vowel in found:
         print(vowel)
 
-def lesson_four():
+
+def lesson_4():
     phrase = "Don't panic"
     plist = list(phrase)
     print(phrase)
@@ -72,29 +76,45 @@ def lesson_four():
     print(plist)
     print(new_phrase)
 
-def lesson_five():
+
+def lesson_5():
     phrase = "Don't panic"
     plist = list(phrase)
     print(phrase)
     print(plist)
-    new_phrase = ''.join(phrase[1:3]+
-                         phrase[-6:3:-1]+
+    new_phrase = ''.join(phrase[1:3] +
+                         phrase[-6:3:-1] +
                          phrase[-4:5:-1])
     print(plist)
     print(new_phrase)
 
-def lesson_six():
+
+def lesson_6():
     paranoid_android = "Marvin"
     letters = list(paranoid_android)
     for char in letters:
-        print char
+        print(char)
 
-def lesson_six_2():
+
+def lesson_6_2():
     paranoid_android = "Marvin, the Paranoid Andorid"
     letters = list(paranoid_android)
     for char in letters[:6]:
-        print '\t', char
+        print('\t', char)
     for char in letters[-7:]:
-        print '\t'*2, char
+        print('\t' * 2, char)
     for char in letters[12:20]:
-        print '\t'*3, char
+        print('\t' * 3, char)
+
+
+def lesson_7():
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+    word = input('Provide a word to search for vowels: ')
+    found = {}
+    for letters in word:
+        if letters in vowels:
+            found.setdefault(letters, 0)
+            found[letters] += 1
+
+    for k, v in sorted(found.items()):
+        print(k, 'was found', v, 'times.')
