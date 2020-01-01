6e75c8a056634a3a6b3d7abf45659e2e9466adf4
Ivan Novikov <ivan.novikov.1990@list.ru>
2020-01-01 19:41:58 +0300

    lesson 4

--- matplotlib/lesson_1.py
+++ matplotlib/lesson_1.py
@@ -54,4 +54,39 @@ def figure_3():
 
     plt.show()
 
-figure_3()
+def figure_4():
+    N = 100
+    n = np.sqrt(N)
+    x = np.arange(n)
+    z = np.random.random(N).reshape(n, n)
+    y = z[5,:]
+
+    fig = plt.figure()
+    cc = plt.contourf(z, alpha=0.5)
+    plt.plot(x, y, label='line', color='red')
+
+    plt.title('1a. Title')
+    plt.xlabel('2a. Xlabel')
+    plt.ylabel('3a. Ylabel')
+    plt.legend()
+    cbar = plt.colorbar(cc)
+
+    plt.text(2.5, 7, '1. Axes', fontsize=26, bbox=dict(edgecolor='w', color='w'))
+    plt.text(4, -0.5, '2. XAxis', fontsize=18, bbox=dict(edgecolor='w', color='w'))
+    plt.text(-0.5, 3.8, '3. YAxis', fontsize=18, bbox=dict(edgecolor='w', color='w'), rotation=90)
+    plt.text(6.3, 7.2, '4. Legend', fontsize=16, bbox=dict(edgecolor='w', color='w'))
+    plt.text(9.1, 5., '5. Colorbar', fontsize=16, bbox=dict(edgecolor='w', color='w'), rotation=90)
+    plt.text(7., 0.8, '6. Xticks', fontsize=12, bbox=dict(edgecolor='w', color='w'))
+    plt.text(0.8, 8.4, '7. Yticks', fontsize=12, bbox=dict(edgecolor='w', color='w'), rotation=90)
+
+    cbar.ax.set_xlabel('5a. Colorbar Xlabel', color='k', rotation=30)
+    cbar.ax.set_ylabel('5b. Colorbar Ylabel', color='k')
+
+    plt.text(2.8, 4.8,'6. Grid lines', fontsize=14)
+    plt.grid(True)
+
+    save('pic_1_5_2', fmt='png')
+
+    plt.show()
+
+figure_4()
