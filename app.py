d8eed2a75c62316fb8927b0470b01c174c838bc9
Ivan <ivan.novikov.1990@list.ru>
2020-08-23 23:05:40 +0300

    test

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-	return "Flask App!"
+def home():
+	return "Hi"
 
 if __name__ == "__main__":
-	app.run(host='127.0.0.1')
+	app.run()
