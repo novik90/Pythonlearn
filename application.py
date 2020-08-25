8a7d13321b1ce4507566f729e0d34c2a20916d94
Ivan <ivan.novikov.1990@list.ru>
2020-08-25 17:40:58 +0300

    pages

--- application.py
+++ application.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/page')
+def page():
+	return render_template('page.html')
+
+@app.route('/work/<id>')
+def work(id):
+	return "Work with id {}".format(id)
+
 if __name__ == '__main__':
 	app.run()
