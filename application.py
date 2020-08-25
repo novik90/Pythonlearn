c9d64dd4fadab482ac035ed354ede89f0e769dba
Ivan <ivan.novikov.1990@list.ru>
2020-08-25 17:02:48 +0300

    test

--- application.py
+++ application.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
+@app.route("/")
 def home():
-	return render_template('index.html')
+	return render_template(`index.html`)
 
 if __name__ == '__main__':
 	app.run()
