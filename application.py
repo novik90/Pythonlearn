cc6d3559e3ae4c189f3828e206575aaf0fcfa967
Ivan <ivan.novikov.1990@list.ru>
2020-08-25 20:36:24 +0300

    Cat dog

--- application.py
+++ application.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
 	us_ag = request.user_agent
-	return render_template('index.html', us_ag = us_ag)
+	lanchaa = "Cat" if str(us_ag) == "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15" else "Dog"
+	return render_template('index.html', us_ag = us_ag, lanchaa = lanchaa)
 
 @app.route('/page')
 def page():
